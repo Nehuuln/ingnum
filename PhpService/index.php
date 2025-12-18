ecda6c9e2522405ea7a679a9c59101347cb8b5c4
Nehuuln <maximeye1234@gmail.com>
2025-12-18 10:12:15 +0100

    add phpservice & dockerfile

---
+++ PhpService/index.php
@@ -0,0 +1,13 @@
+<?php
+$prenom = 'Maxime';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <p>Je suis <?php echo $prenom; ?></p>
+</body>
+</html>
